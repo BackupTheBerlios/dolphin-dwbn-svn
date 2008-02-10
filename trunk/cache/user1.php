0cd541ad630de40567d0b3dd4b2b9179f8c1487c
klimpong <klimpong@ce1eed5e-d145-0410-9b82-bb00238a9ea4>
2008-02-10 19:12:13 +0000

    * initial checkin

---
+++ trunk/cache/user1.php
@@ -0,0 +1,32 @@
+<?
+
+$aUser[1] = array();
+$aUser[1]['datafile'] = true;
+$aUser[1]['ID'] = '1';
+$aUser[1]['NickName'] = 'till';
+$aUser[1]['Password'] = 'f8b0139c2e17a8fc2f7d791374133bd4';
+$aUser[1]['Country'] = 'DE';
+$aUser[1]['City'] = 'Berlin';
+$aUser[1]['Headline'] = 'Achtung, Spaß!';
+$aUser[1]['DescriptionMe'] = 'Till ist immer schon vorallem Till. ';
+$aUser[1]['Sex'] = 'male';
+$aUser[1]['DateOfBirth'] = '1982-03-05';
+$aUser[1]['Email'] = 'klimpong@gmail.com';
+$aUser[1]['EmailFlag'] = '';
+$aUser[1]['EmailNotify'] = '';
+$aUser[1]['Status'] = 'Active';
+$aUser[1]['LastLoggedIn'] = '02-10-08 00:08';
+$aUser[1]['LastModified'] = '00-00-00 00:00';
+$aUser[1]['LastReg'] = '2008-02-09 19:52:19';
+$aUser[1]['LookingFor'] = 'all';
+$aUser[1]['PrimPhoto'] = '0';
+$aUser[1]['Picture'] = '0';
+$aUser[1]['LastNavTime'] = '2008-02-10 00:08:48';
+$aUser[1]['LastNavPage'] = '6';
+$aUser[1]['Featured'] = '0';
+$aUser[1]['aff_num'] = '0';
+$aUser[1]['zip'] = '';
+$aUser[1]['PaymentSubscription'] = '0';
+$aUser[1]['Tags'] = 'berlin, till';
+
+?>
---
+++ trunk/cache/user2.php
@@ -0,0 +1,32 @@
+<?
+
+$aUser[2] = array();
+$aUser[2]['datafile'] = true;
+$aUser[2]['ID'] = '2';
+$aUser[2]['NickName'] = 'Stephan';
+$aUser[2]['Password'] = 'f3d51e7f439d108059345d4a74a3d931';
+$aUser[2]['Country'] = 'DE';
+$aUser[2]['City'] = 'Munich';
+$aUser[2]['Headline'] = 'Wer später bremst bleibt länger schnell!';
+$aUser[2]['DescriptionMe'] = 'Raum=Kaffee';
+$aUser[2]['Sex'] = 'male';
+$aUser[2]['DateOfBirth'] = '1971-12-10';
+$aUser[2]['Email'] = 'stephan@mahamudra.de';
+$aUser[2]['EmailFlag'] = 'HTML';
+$aUser[2]['EmailNotify'] = 'NotifyMe';
+$aUser[2]['Status'] = 'Active';
+$aUser[2]['LastLoggedIn'] = '02-10-08 11:36';
+$aUser[2]['LastModified'] = '02-09-08 21:44';
+$aUser[2]['LastReg'] = '0000-00-00 00:00:00';
+$aUser[2]['LookingFor'] = 'all';
+$aUser[2]['PrimPhoto'] = '0';
+$aUser[2]['Picture'] = '0';
+$aUser[2]['LastNavTime'] = '2008-02-10 11:36:08';
+$aUser[2]['LastNavPage'] = '6';
+$aUser[2]['Featured'] = '0';
+$aUser[2]['aff_num'] = '0';
+$aUser[2]['zip'] = '80636';
+$aUser[2]['PaymentSubscription'] = '0';
+$aUser[2]['Tags'] = 'Lama rocks!';
+
+?>
