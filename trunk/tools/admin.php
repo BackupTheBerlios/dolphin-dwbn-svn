96a8d56a17b7b9e5270afa499562a0dd27b02bb1
irek <irek@ce1eed5e-d145-0410-9b82-bb00238a9ea4>
2008-09-25 21:35:34 +0000

    commiting not working version - to get Andre's help

--- trunk/tools/admin.php
+++ trunk/tools/admin.php
@@ -1,6 +1,8 @@
 <?php
 
-require_once('../virtualsangha/inc/header.inc.php');
+set_include_path( '../virtualsangha' . PATH_SEPARATOR . get_include_path() );
+
+require_once('inc/header.inc.php');
 
 $ioc = new IocContainer();
 $admin = $ioc->getInstanceOf('Admin');
